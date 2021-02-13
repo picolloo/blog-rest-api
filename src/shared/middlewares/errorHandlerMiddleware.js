7691afe19b57ddb4df32b75fce64c02e2b0e0d78
picolloo <lucaspicolloo@gmail.com>
2021-02-13 15:24:10 -0300

    fix: error handling loggin error

--- src/shared/middlewares/errorHandlerMiddleware.js
+++ src/shared/middlewares/errorHandlerMiddleware.js
@@ -1,6 +1,8 @@
 import { ENV } from '../../config';
 
 export default function (error, request, response, next) {
+  console.error({ error });
+
   const statusCode = error.statusCode === 200 ? 500 : error.statusCode;
   response.status(statusCode);
 
